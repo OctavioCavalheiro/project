9bf42b98e3260b2b702a9655c3fcc03f3fa6e0a9
Octavio Cavalheiro <octavioandre.cavalheiro@gmail.com>
2024-04-29 18:26:47 -0300

    Customer repository, service and controller created

---
+++ src/main/java/com/octavio/project/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.octavio.project.repository;
+
+import com.octavio.project.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ src/main/java/com/octavio/project/service/CustomerService.java
@@ -0,0 +1,24 @@
+package com.octavio.project.service;
+
+import com.octavio.project.entity.Customer;
+import com.octavio.project.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    //Variable customerRepository, dependency injection
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Customer> getAllCustomers(){
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerById(Long id){
+        return customerRepository.findById(id).orElse(null);
+    }
+}
